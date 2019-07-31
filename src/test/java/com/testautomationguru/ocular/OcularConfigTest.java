4bc6a2de7df28cad9eda3cb8bb87495800d97180
Alexey <andreikin.fingers@gmail.com>
2019-07-31 12:10:26 +0300

    fixed typos, applied code formatting, fixed tests

--- src/test/java/com/testautomationguru/ocular/OcularConfigTest.java
+++ src/test/java/com/testautomationguru/ocular/OcularConfigTest.java
@@ -1,61 +1,56 @@
 package com.testautomationguru.ocular;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
+import com.testautomationguru.ocular.comparator.OcularResult;
+import com.testautomationguru.ocular.exception.OcularException;
+import com.testautomationguru.ocular.pages.*;
+import com.testautomationguru.ocular.snapshot.SnapshotBuilder;
+import com.testautomationguru.ocular.snapshot.SnapshotBuilderImpl;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import com.testautomationguru.ocular.comparator.OcularResult;
-import com.testautomationguru.ocular.exception.OcularException;
-import com.testautomationguru.ocular.pages.RichFaceTheme;
-import com.testautomationguru.ocular.pages.RichFaceWithSimilarity;
-import com.testautomationguru.ocular.pages.SnapPage;
-import com.testautomationguru.ocular.pages.SnapPageNoAnnotation;
-import com.testautomationguru.ocular.pages.SnapPageWithSimilarity;
-import com.testautomationguru.ocular.snapshot.SnapshotBuilder;
-import com.testautomationguru.ocular.snapshot.SnapshotBuilderImpl;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class OcularConfigTest {
-    
+
     @BeforeTest
     public void ocularConfigSetup() {
         Ocular.config().reset();
     }
-    
+
     @Test
     public void defaultConfigTest() {
         Assert.assertEquals(100, Ocular.config().getGlobalSimilarity());
-        Assert.assertEquals(null, Ocular.config().getResultPath());
-        Assert.assertEquals(null, Ocular.config().getSnapshotPath());
+        Assert.assertNull(Ocular.config().getResultPath());
+        Assert.assertNull(Ocular.config().getSnapshotPath());
         Assert.assertTrue(Ocular.config().canSaveSnapshot());
     }
 
-    @Test(dependsOnMethods = { "defaultConfigTest" })
+    @Test(dependsOnMethods = {"defaultConfigTest"})
     public void configOverrideTest() throws IOException {
         Ocular.config()
-               .resultPath(Paths.get("."))
-               .snapshotPath(Paths.get("."))
-               .globalSimilarity(10)
-               .saveSnapshot(false);
+                .resultPath(Paths.get("."))
+                .snapshotPath(Paths.get("."))
+                .globalSimilarity(10)
+                .saveSnapshot(false);
 
         Assert.assertEquals(10, Ocular.config().getGlobalSimilarity());
         Assert.assertEquals(0,
-            Paths.get(System.getProperty("user.dir")).compareTo(Ocular.config().getResultPath().toRealPath()));
+                Paths.get(System.getProperty("user.dir")).compareTo(Ocular.config().getResultPath().toRealPath()));
         Assert.assertEquals(0,
-            Paths.get(System.getProperty("user.dir")).compareTo(Ocular.config().getSnapshotPath().toRealPath()));
+                Paths.get(System.getProperty("user.dir")).compareTo(Ocular.config().getSnapshotPath().toRealPath()));
         Assert.assertFalse(Ocular.config().canSaveSnapshot());
-        
+
         Ocular.config().globalSimilarity(100)
-                      .resultPath(Paths.get(".", "target"))
-                      .saveSnapshot(true);
+                .resultPath(Paths.get(".", "target"))
+                .saveSnapshot(true);
 
     }
 
-    @Test(dependsOnMethods = { "configOverrideTest" }, expectedExceptions = OcularException.class)
-    public void snapNoAnnationTest() {
+    @Test(dependsOnMethods = {"configOverrideTest"}, expectedExceptions = OcularException.class)
+    public void snapNoAnnotationTest() {
         SnapshotBuilder snapshot = new SnapshotBuilderImpl();
         snapshot.from(SnapPageNoAnnotation.class);
 
@@ -63,79 +58,76 @@ public class OcularConfigTest {
         snapshot.from(sno);
     }
 
-    @Test(dependsOnMethods = { "snapNoAnnationTest" })
-    public void snapAnnationTest() {
+    @Test(dependsOnMethods = {"snapNoAnnotationTest"})
+    public void snapAnnotationTest() {
         SnapshotBuilder snapshot = new SnapshotBuilderImpl();
         snapshot.from(SnapPage.class);
         snapshot.from(SnapPageWithSimilarity.class);
     }
 
-    @Test(dependsOnMethods = { "snapAnnationTest" })
+    @Test(dependsOnMethods = {"snapAnnotationTest"})
     public void ocularCompareUsingPath() {
-        
         Path snapshotPath = Paths.get(System.getProperty("user.dir"), "src/test/resources/image-compare/RichFacesTheme-blueSky.png");
         Path samplePath = Paths.get(System.getProperty("user.dir"), "src/test/resources/image-compare/RichFacesTheme-classic.png");
-               
+
         OcularResult result = Ocular.snapshot()
-            .from(snapshotPath)
-            .sample()
-            .using(samplePath)
-            .compare();
-        
+                .from(snapshotPath)
+                .sample()
+                .using(samplePath)
+                .compare();
+
         Assert.assertFalse(result.isEqualsImages());
         Assert.assertEquals(99, result.getSimilarity());
-        
+
         result = Ocular.snapshot()
-            .from(snapshotPath)
-            .sample()
-            .using(samplePath)
-            .similarity(98)
-            .compare();
-        
+                .from(snapshotPath)
+                .sample()
+                .using(samplePath)
+                .similarity(98)
+                .compare();
+
         Assert.assertTrue(result.isEqualsImages());
-        Assert.assertEquals(99, result.getSimilarity());        
+        Assert.assertEquals(99, result.getSimilarity());
     }
-    
-    @Test(dependsOnMethods = { "ocularCompareUsingPath" })
+
+    @Test(dependsOnMethods = {"ocularCompareUsingPath"})
     public void ocularCompareUsingSnap() {
-        
         Path samplePath = Paths.get(System.getProperty("user.dir"), "src/test/resources/image-compare/RichFacesTheme-blueSky.png");
-        
+
         OcularResult result = Ocular.snapshot()
-            .from(RichFaceTheme.class)
-            .replaceAttribute("THEME", "blueSky")
-            .sample()
-            .using(samplePath)
-            .compare();
-        
+                .from(RichFaceTheme.class)
+                .replaceAttribute("THEME", "blueSky")
+                .sample()
+                .using(samplePath)
+                .compare();
+
         Assert.assertTrue(result.isEqualsImages());
-        Assert.assertEquals(100, result.getSimilarity());        
-        
+        Assert.assertEquals(100, result.getSimilarity());
+
         Ocular.config().globalSimilarity(100);
         result = Ocular.snapshot()
-            .from(RichFaceTheme.class)
-            .replaceAttribute("THEME", "classic")
-            .sample()
-            .using(samplePath)
-            .compare();
-        
+                .from(RichFaceTheme.class)
+                .replaceAttribute("THEME", "classic")
+                .sample()
+                .using(samplePath)
+                .compare();
+
         Assert.assertFalse(result.isEqualsImages());
-        Assert.assertEquals(99, result.getSimilarity());  
-        
-    }  
-    
-    @Test(dependsOnMethods = { "ocularCompareUsingSnap" })
+        Assert.assertEquals(99, result.getSimilarity());
+
+    }
+
+    @Test(dependsOnMethods = {"ocularCompareUsingSnap"})
     public void ocularCompareUsingSnapSimilarity() {
-        
         Path samplePath = Paths.get(System.getProperty("user.dir"), "src/test/resources/image-compare/RichFacesTheme-classic.png");
-        
+
         OcularResult result = Ocular.snapshot()
-            .from(RichFaceWithSimilarity.class)
-            .sample()
-            .using(samplePath)
-            .compare();
-        
+                .from(RichFaceWithSimilarity.class)
+                .sample()
+                .using(samplePath)
+                .compare();
+
         Assert.assertTrue(result.isEqualsImages());
         Assert.assertEquals(99, result.getSimilarity());
-    }      
+    }
 }
